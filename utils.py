cb2b785ec7bd6f3adb97d7e9332a783f58918b45
keivan alizadeh <keivan.alizadeh@gmail.com>
2020-05-18 07:15:37 -0700

    Fixing some issues :)

--- utils.py
+++ utils.py
@@ -42,8 +42,8 @@ def euclidean_metric(a, b):
 
 def cosine_sim(a, b):
     eps = 1e-10
-    a_norm = (a.norm(dim=1)[:, None] + eps)
-    b_norm = (b.norm(dim=1)[:, None] + eps)
+    a_norm = a / (a.norm(dim=1)[:, None] + eps)
+    b_norm = b / (b.norm(dim=1)[:, None] + eps)
     res = torch.mm(a_norm, b_norm.transpose(0, 1))
     return torch.mm(a, b.transpose(0, 1)), res
 
