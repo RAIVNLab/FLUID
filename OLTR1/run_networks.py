7a16b98975f5e245aeb638ed8cf2f114c31d8159
keivan alizadeh <keivan.alizadeh@gmail.com>
2020-05-30 21:34:08 -0700

    OLTR1.utils instead utils

--- OLTR1/run_networks.py
+++ OLTR1/run_networks.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from tqdm import tqdm
-from utils import *
+from OLTR1.utils import *
 import time
 import numpy as np
 import warnings
