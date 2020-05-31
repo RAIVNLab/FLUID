0e6df117cb61ff66003be2a477a5583e03d572fd
keivan alizadeh <keivan.alizadeh@gmail.com>
2020-05-30 23:16:38 -0700

    Remove redundant import.

--- models.py
+++ models.py
@@ -9,7 +9,6 @@ from convnet import Convnet
 import sys
 # sys.path.insert(0, "pytorchmaml/")
 # from pytorchmaml.maml.model import ModelConvOmniglot, ModelConvMiniImagenet
-sys.path.insert(0, os.path.abspath("OLTR1/"))
 from OLTR1.run_networks import model as oltr_model
 from OLTR1.utils import source_import
 
