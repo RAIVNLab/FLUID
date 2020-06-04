5622eb8834ce3ee1484bbbb837827f3f6f3f81c0
keivan alizadeh <keivan.alizadeh@gmail.com>
2020-06-03 20:59:21 -0700

    Moco.

--- OLTR1/models/ResNet18_mocoFeature.py
+++ OLTR1/models/ResNet18_mocoFeature.py
@@ -3,7 +3,7 @@ from OLTR1.utils import *
 
 def create_model(use_modulatedatt=False, use_fc=False, dropout=None, stage1_weights=False, dataset=None, test=False, *args):
     
-    print('Loading Scratch ResNet 18 Feature Model.')
+    print('Loading Scratch ResNet 18 moco Feature Model.')
     model = PreTrainMocoResnet(BasicBlock, 18, use_modulatedatt=use_modulatedatt, use_fc=use_fc, dropout=None)
 
     return model
