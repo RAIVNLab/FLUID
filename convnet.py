bd850b28fcd3a5801f791b9538ba13f0076c4056
MattWallingford <mcw244@cs.washington.edu>
2022-03-05 15:38:08 -0800

    added ProtoMaml and made fixes

--- convnet.py
+++ convnet.py
@@ -1,4 +1,10 @@
+import torch
+import torchvision
 import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as data
+import torch.optim as optim
+import pytorch_lightning as pl
 
 def conv_block(in_channels, out_channels):
     bn = nn.BatchNorm2d(out_channels)
@@ -9,6 +15,15 @@ def conv_block(in_channels, out_channels):
         nn.ReLU(),
         nn.MaxPool2d(2)
     )
+
+def get_convnet(output_size):
+    convnet = torchvision.models.DenseNet(growth_rate=32,
+                                          block_config=(6, 6, 6, 6),
+                                          bn_size=2,
+                                          num_init_features=64,
+                                          num_classes=output_size  # Output dimensionality
+                                         )
+    return convnet
 
 
 class Convnet(nn.Module):
@@ -29,3 +44,111 @@ class Convnet(nn.Module):
         
     def __str__(self):
         return 'Convnet'
+
+class ProtoMAML(pl.LightningModule):
+
+    def __init__(self, proto_dim, lr, lr_inner, lr_output, num_inner_steps):
+        """
+        Inputs
+            proto_dim - Dimensionality of prototype feature space
+            lr - Learning rate of the outer loop Adam optimizer
+            lr_inner - Learning rate of the inner loop SGD optimizer
+            lr_output - Learning rate for the output layer in the inner loop
+            num_inner_steps - Number of inner loop updates to perform
+        """
+        super().__init__()
+        self.save_hyperparameters()
+        self.model = get_convnet(output_size=self.hparams.proto_dim)
+
+    def configure_optimizers(self):
+        optimizer = optim.AdamW(self.parameters(), lr=self.hparams.lr)
+        scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[140,180], gamma=0.1)
+        return [optimizer], [scheduler]
+
+    def run_model(self, local_model, output_weight, output_bias, imgs, labels):
+        # Execute a model with given output layer weights and inputs
+        feats = local_model(imgs)
+        preds = F.linear(feats, output_weight, output_bias)
+        loss = F.cross_entropy(preds, labels)
+        acc = (preds.argmax(dim=1) == labels).float()
+        return loss, preds, acc
+
+    def adapt_few_shot(self, support_imgs, support_targets):
+        # Determine prototype initialization
+        support_feats = self.model(support_imgs)
+        prototypes, classes = ProtoNet.calculate_prototypes(support_feats, support_targets)
+        support_labels = (classes[None,:] == support_targets[:,None]).long().argmax(dim=-1)
+        # Create inner-loop model and optimizer
+        local_model = deepcopy(self.model)
+        local_model.train()
+        local_optim = optim.SGD(local_model.parameters(), lr=self.hparams.lr_inner)
+        local_optim.zero_grad()
+        # Create output layer weights with prototype-based initialization
+        init_weight = 2 * prototypes
+        init_bias = -torch.norm(prototypes, dim=1)**2
+        output_weight = init_weight.detach().requires_grad_()
+        output_bias = init_bias.detach().requires_grad_()
+
+        # Optimize inner loop model on support set
+        for _ in range(self.hparams.num_inner_steps):
+            # Determine loss on the support set
+            loss, _, _ = self.run_model(local_model, output_weight, output_bias, support_imgs, support_labels)
+            # Calculate gradients and perform inner loop update
+            loss.backward()
+            local_optim.step()
+            # Update output layer via SGD
+            output_weight.data -= self.hparams.lr_output * output_weight.grad
+            output_bias.data -= self.hparams.lr_output * output_bias.grad
+            # Reset gradients
+            local_optim.zero_grad()
+            output_weight.grad.fill_(0)
+            output_bias.grad.fill_(0)
+
+        # Re-attach computation graph of prototypes
+        output_weight = (output_weight - init_weight).detach() + init_weight
+        output_bias = (output_bias - init_bias).detach() + init_bias
+
+        return local_model, output_weight, output_bias, classes
+
+    def outer_loop(self, batch, mode="train"):
+        accuracies = []
+        losses = []
+        self.model.zero_grad()
+
+        # Determine gradients for batch of tasks
+        for task_batch in batch:
+            imgs, targets = task_batch
+            support_imgs, query_imgs, support_targets, query_targets = split_batch(imgs, targets)
+            # Perform inner loop adaptation
+            local_model, output_weight, output_bias, classes = self.adapt_few_shot(support_imgs, support_targets)
+            # Determine loss of query set
+            query_labels = (classes[None,:] == query_targets[:,None]).long().argmax(dim=-1)
+            loss, preds, acc = self.run_model(local_model, output_weight, output_bias, query_imgs, query_labels)
+            # Calculate gradients for query set loss
+            if mode == "train":
+                loss.backward()
+
+                for p_global, p_local in zip(self.model.parameters(), local_model.parameters()):
+                    p_global.grad += p_local.grad  # First-order approx. -> add gradients of finetuned and base model
+
+            accuracies.append(acc.mean().detach())
+            losses.append(loss.detach())
+
+        # Perform update of base model
+        if mode == "train":
+            opt = self.optimizers()
+            opt.step()
+            opt.zero_grad()
+
+        self.log(f"{mode}_loss", sum(losses) / len(losses))
+        self.log(f"{mode}_acc", sum(accuracies) / len(accuracies))
+
+    def training_step(self, batch, batch_idx):
+        self.outer_loop(batch, mode="train")
+        return None  # Returning None means we skip the default training optimizer steps by PyTorch Lightning
+
+    def validation_step(self, batch, batch_idx):
+        # Validation requires to finetune a model, hence we need to enable gradients
+        torch.set_grad_enabled(True)
+        self.outer_loop(batch, mode="val")
+        torch.set_grad_enabled(False)
--- options.py
+++ options.py
@@ -11,7 +11,7 @@ class Options():
         offline.add_argument('--m', type=float, default=0.1)
         offline.add_argument('--num_layers', type=int, default=1,
                              help='Number of layers to fine-tune')
-        offline.add_argument('--epochs', type=int, default=2)
+        offline.add_argument('--epochs', type=int, default=4)
         offline.add_argument('--offline_batch_size', type=int, default=256)
         offline.add_argument('--batch_factor', type=int, default=2)
         offline.add_argument('--trainer', type=str, default='batch')
